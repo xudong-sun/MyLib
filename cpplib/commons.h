459be0d3e5314022d309cd440b8bbc8b28cdf8cf
xudong-sun <sunxudong@pku.edu.cn>
2017-06-25 16:28:24 +0800

    add toString

--- cpplib/commons.h
+++ cpplib/commons.h
@@ -7,13 +7,22 @@ namespace commons {
 		system("pause");
 	}
 
-	// join
+	// similar join function as in python
 	template <typename T>
 	std::string join(std::vector<T> vec, std::string separator = " ") {
 		if (vec.size() == 0) return "";
 		std::stringstream ss;
 		ss << vec[0];
 		for (size_t i = 1; i < vec.size(); i++) ss << separator << vec[i];
+		return ss.str();
+	}
+
+	// convert any type to std::string
+	// for any type that has no built-in operator<< with std::ostream, please first checkout helpers in "print_utils.h"
+	template <typename T>
+	std::string toString(T v) {
+		std::stringstream ss;
+		ss << v;
 		return ss.str();
 	}
 
